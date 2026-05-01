437018701734b9f646383cb69bf9e0617b2454e2
jwatson <jwatson2396@gmail.com>
2026-05-01 16:56:25 -0400

    working with cli input

--- src/main.c
+++ src/main.c
@@ -2,4 +2,13 @@
 
 int main() {
 	printf("Hello, World\n");
+
+	char text[10];
+	int result = scanf_s("%s", text);
+	while (result != EOF) {
+		printf("results: %d, input: %s\n", result, text);
+		result = scanf_s("%s", text);
+	}
+
+	return 0;
 }
