0fe21ef2a60bef215c89169ef398d22465de7ed5
makuku <makuku@BLD.localdomain>
2026-06-24 13:54:37 -0400

    got toy version of create table file working

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,68 @@
 const std = @import("std");
 const io = std.Io;
 
+
+fn createTable(init: std.process.Init) !void {
+    const dirName = "zig-out/bin/db";
+    const fileName = "table";
+
+    const process_io = init.io;
+    const cwd: std.Io.Dir = std.Io.Dir.cwd();
+
+    cwd.createDir(process_io, dirName, .default_dir) catch |e| switch (e) {
+        error.PathAlreadyExists => {
+            std.debug.print("Warning: Dir already exists: {s}\n", .{"db"});
+        },
+        else => return e,
+    };
+    
+    var db_dir: std.Io.Dir = try cwd.openDir(process_io, dirName, .{});
+    defer db_dir.close(process_io);
+
+    const file: std.Io.File = try db_dir.createFile(process_io, fileName, .{});
+    defer file.close(process_io);
+
+
+    var file_writer = file.writer(process_io, &.{});
+    const writer = &file_writer.interface;
+
+    const byte_written = try writer.write("It's zigling time!");
+    std.debug.print("Successfully wrote {d} bytes.\n", .{byte_written});
+}
+
 pub fn main(init: std.process.Init) !void {
     var input: [1024]u8 = undefined;
     var output: [1024]u8 = undefined;
 
-    var stdin_reader: io.File.Reader = io.File.stdin().reader(init.io, &input);
-    var stdout_writer: io.File.Writer = io.File.stdout().writer(init.io, &output);
-    var stdin: *io.Reader = &stdin_reader.interface;
-    var stdout: *io.Writer = &stdout_writer.interface;
-
-    try stdout.writeAll("Hello World\n");
-    try stdout.flush();
     while (true) {
-        try stdout.writeAll("Enter your name: ");
+        var stdin_reader: io.File.Reader = io.File.stdin().reader(init.io, &input);
+        var stdout_writer: io.File.Writer = io.File.stdout().writer(init.io, &output);
+
+        var stdin: *io.Reader = &stdin_reader.interface;
+        var stdout: *io.Writer = &stdout_writer.interface;
+
+        try stdout.writeAll("Hello World\n");
         try stdout.flush();
 
-        var name: []u8 = stdin.takeDelimiterExclusive('\n') catch |err| switch (err) {
-            error.EndOfStream => break, // Ctrl-D / Ctrl-Z
+        try stdout.writeAll("Enter cmd: ");
+        try stdout.flush();
+
+        const cmd: []u8 = stdin.takeDelimiterExclusive('\n') catch |err| switch (err) {
+            error.EndOfStream => break,
             else => return err,
         };
 
-        if (name.len == 0) break;
+        if (cmd.len == 0) {
+            try stdout.writeAll("entered blank cmd\n");
+            try stdout.flush();
+            break;
+        }
 
-        try stdout.print("Your name is: {s}\n", .{name});
-        try stdout.flush();
+        if (std.ascii.eqlIgnoreCase(cmd, "create-table")) {
+            try createTable(init);
+            try stdout.writeAll("created table\n");
+            try stdout.flush();
+        }
+
     }
-
-    try stdout.writeAll("\nDone.");
-    try stdout.flush();
 }
